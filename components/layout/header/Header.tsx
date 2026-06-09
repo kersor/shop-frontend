3796fd67bf3bccc710b07ccfbfcdc955181097be
svolochyo <vasilev.rustik.03@mail.ru>
2026-06-09 14:22:29 +0300

    add header

---
+++ components/layout/header/Header.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Link from 'next/link'
+import { Avatar, AvatarImage } from '@/components/ui/avatar'
+import { Input } from '@/components/ui/input'
+import { ShoppingBag, User } from 'lucide-react'
+
+const Header = () => {
+
+    return (
+        <header className="ml-auto mr-auto w-full max-w-7xl px-5">
+            <div className="bg-secondary border border-border shadow-sm py-5 px-8 rounded-xl">
+                <div className='flex items-center justify-between'>
+                    <div className='flex items-center gap-20'>
+                        <Link className='uppercase text-2xl font-black tracking-tight text-foreground' href='/'>Shop</Link>
+                        <Input className='min-w-75 bg-white' placeholder='Введите название товара...' />
+                    </div>
+
+                    <div className='flex items-center gap-5'>
+                        <Link href='/cart' className='flex justify-center items-center border rounded-full p-1 w-10 h-10 bg-white'>
+                            <ShoppingBag strokeWidth={1} size={20}/>
+                        </Link>
+                        <Link href='/profile' className='bg-white rounded-full w-10 h-10'>
+                            <Avatar className='flex justify-center items-center' size='lg'>
+                                <User strokeWidth={1} size={20}/>
+                            </Avatar>
+                        </Link>
+                    </div>
+                    {/* <Theme /> */}
+
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
