932a098212bbfd56880f6075c04644dabfbea3b4
svolochyo <vasilev.rustik.03@mail.ru>
2026-06-10 19:14:12 +0300

    hot fix

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -95,14 +95,22 @@ function Carousel({
 
   React.useEffect(() => {
     if (!api) return
-    onSelect(api)
-    api.on("reInit", onSelect)
-    api.on("select", onSelect)
+
+    const handleSelect = () => {
+      setCanScrollPrev(api.canScrollPrev())
+      setCanScrollNext(api.canScrollNext())
+    }
+
+    handleSelect()
+
+    api.on("reInit", handleSelect)
+    api.on("select", handleSelect)
 
     return () => {
-      api?.off("select", onSelect)
+      api.off("reInit", handleSelect)
+      api.off("select", handleSelect)
     }
-  }, [api, onSelect])
+  }, [api])
 
   return (
     <CarouselContext.Provider
