6efa3f3d88b33904e00ffd24de176ef7492d4a1e
kersor <vasilev.rustik.03@mail.ru>
2026-09-12 19:41:10 +0300

    add files

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
+  output: "standalone",
+  env: {
+    NEXT_PUBLIC_API_URL: isProduction
+      ? process.env.NEXT_PUBLIC_API_URL_PROD
+      : process.env.NEXT_PUBLIC_API_URL_DEV,
+  },
   images: {
     remotePatterns: [
       {
